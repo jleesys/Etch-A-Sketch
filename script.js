34f05e5c4fb528b108da6bac89f5c50085269908
jleesys949 <jlee.post24@gmail.com>
2022-09-15 17:54:23 -0700

    Add comment

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const boardView = document.querySelector('.board');
 let tempClass = "";
 
+// creates a board of 16 squares
+// organized / styled in css stylesheet
 const initializeBoard = () => {
     for (let i = 0; i < 16; i++) {
         const newDiv = document.createElement('div');
